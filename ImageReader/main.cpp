591009a89d7132b6edb781eb2d357f46644d3942
liulun <xland@live.cn>
2026-08-03 19:00:15 +0800

    Update main.cpp

--- ImageReader/main.cpp
+++ ImageReader/main.cpp
@@ -1,7 +1,14 @@
 ﻿#include "pch.h"
 #include "WindowMain.h"
 
-
+/// <summary>
+/// orc对象应该缓存 ImgViewer::readImg
+/// </summary>
+/// <param name="hInstance"></param>
+/// <param name="hPrevInstance"></param>
+/// <param name="lpCmdLine"></param>
+/// <param name="nCmdShow"></param>
+/// <returns></returns>
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
     WindowMain::init();
